8894d2e08bb0b93c1a815e2b0ea60ba928739f73
Japheth Oruko <japhethoruko@gmail.com>
2024-11-12 20:00:39 +0300

    update

--- components/navbar.js
+++ components/navbar.js
@@ -1,15 +1,19 @@
-
 class Navbar extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `
+  connectedCallback() {
+    const basePath = window.location.pathname.includes(
+      "multipage_website_updated"
+    )
+      ? "/multipage_website_updated"
+      : "";
+    this.innerHTML = `
             <nav>
                 <ul>
-                    <li><a href="/index.html">Home</a></li>
-                    <li><a href="/pages/about.html">About</a></li>
-                    <li><a href="/pages/contact.html">Contact</a></li>
+                    <li><a href="${basePath}/index.html">Home</a></li>
+                    <li><a href="${basePath}/pages/about.html">About</a></li>
+                    <li><a href="${basePath}/pages/contact.html">Contact</a></li>
                 </ul>
             </nav>
         `;
-    }
+  }
 }
-customElements.define('navbar-component', Navbar);
+customElements.define("navbar-component", Navbar);
